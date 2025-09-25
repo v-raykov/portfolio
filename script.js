5e1fa5b97660be06a6868fc0845c92e7bd00a2ef
v-raykov <v03a01raik@pm.me>
2025-09-25 11:11:44 +0300

    separate header and footer components

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  fetch('components/header.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('header-placeholder').innerHTML = html;
+
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    const navLinks = document.querySelectorAll('#header-placeholder nav a');
+
+    navLinks.forEach(link => {
+      if (link.getAttribute('href') === currentPage) {
+        link.classList.add('active');
+      }
+    });
+  })
+  .catch(error => console.error('Error loading the header:', error));
+
+  fetch('components/footer.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('footer-placeholder').innerHTML = html;
+  })
+  .catch(error => console.error('Error loading the footer:', error));
+});
